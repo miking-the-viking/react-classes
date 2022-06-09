263d1087e36191de02bad9045ca7ae8a88fb7565
macv93 <m.canarte@vehikl.com>
2022-06-09 11:53:00 -0400

    Refactor callback hell to async call

--- apps/classes/src/app/app.tsx
+++ apps/classes/src/app/app.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import TickerInput from './components/TickerInput';
 
 const API_TOKEN = '4KaoqFwNmqgFV5P4ui8XQZydpb1UivieepU82edE';
-class App extends React.Component<any, { data: any }> {
+class App extends React.Component<any, { data: any; loading: boolean }> {
   constructor(props: any) {
     super(props);
     this.submit = this.submit.bind(this);
-    this.state = { data: null };
+    this.state = { data: null, loading: false };
   }
 
   componentDidMount() {
@@ -17,19 +17,24 @@ class App extends React.Component<any, { data: any }> {
   submit(ticker: string) {
     console.log('submitted ticker: ', ticker);
 
-    // make api call
-    // stockdata.org
-    // 4KaoqFwNmqgFV5P4ui8XQZydpb1UivieepU82edE
-    // https://api.stockdata.org/v1/data/quote?symbols=AAPL%2CTSLA%2CMSFT&api_token=4KaoqFwNmqgFV5P4ui8XQZydpb1UivieepU82edE
-
     const url = `https://api.stockdata.org/v1/data/quote?symbols=${ticker}&api_token=${API_TOKEN}`;
-    const response = fetch(url).then((data) =>
-      data.json().then(({ data }) => {
-        console.log('response: ', data);
+    (async () => {
+      this.setState((prev) => ({ ...prev, loading: true }));
+
+      try {
+        const response = await fetch(url);
+        const { data } = await response.json();
         const tickerInformation = data[0];
-        this.setState({ data: tickerInformation });
-      })
-    );
+        this.setState({
+          data: tickerInformation,
+          loading: false,
+        });
+      } catch (e) {
+        console.log('There was an issue: ', e);
+      }
+
+      this.setState((prev) => ({ ...prev, loading: false }));
+    })();
   }
 
   render() {
@@ -37,7 +42,10 @@ class App extends React.Component<any, { data: any }> {
       <div>
         <h1>Stock Data</h1>
         <TickerInput onSubmit={this.submit} />
-        <h1>{JSON.stringify(this.state.data)}</h1>
+        {this.state.loading && <div>Loading...</div>}
+        <h1>
+          {this.state.data && JSON.stringify(this.state.data, null, '\n')}
+        </h1>
       </div>
     );
   }
