0d8d3debb57b9a2beefa141e5d6f401de8470a26
macv93 <m.canarte@vehikl.com>
2022-06-09 11:33:44 -0400

    Retrieve a quote for a specific ticker

--- apps/classes/src/app/app.tsx
+++ apps/classes/src/app/app.tsx
@@ -2,10 +2,12 @@ import styled from 'styled-components';
 import React from 'react';
 import TickerInput from './components/TickerInput';
 
-class App extends React.Component {
+const API_TOKEN = '4KaoqFwNmqgFV5P4ui8XQZydpb1UivieepU82edE';
+class App extends React.Component<any, { data: any }> {
   constructor(props: any) {
     super(props);
     this.submit = this.submit.bind(this);
+    this.state = { data: null };
   }
 
   componentDidMount() {
@@ -14,6 +16,20 @@ class App extends React.Component {
 
   submit(ticker: string) {
     console.log('submitted ticker: ', ticker);
+
+    // make api call
+    // stockdata.org
+    // 4KaoqFwNmqgFV5P4ui8XQZydpb1UivieepU82edE
+    // https://api.stockdata.org/v1/data/quote?symbols=AAPL%2CTSLA%2CMSFT&api_token=4KaoqFwNmqgFV5P4ui8XQZydpb1UivieepU82edE
+
+    const url = `https://api.stockdata.org/v1/data/quote?symbols=${ticker}&api_token=${API_TOKEN}`;
+    const response = fetch(url).then((data) =>
+      data.json().then(({ data }) => {
+        console.log('response: ', data);
+        const tickerInformation = data[0];
+        this.setState({ data: tickerInformation });
+      })
+    );
   }
 
   render() {
@@ -21,6 +37,7 @@ class App extends React.Component {
       <div>
         <h1>Stock Data</h1>
         <TickerInput onSubmit={this.submit} />
+        <h1>{JSON.stringify(this.state.data)}</h1>
       </div>
     );
   }
--- apps/classes/src/app/components/TickerInput.tsx
+++ apps/classes/src/app/components/TickerInput.tsx
@@ -3,21 +3,6 @@ import React from 'react';
 type TickerInputProps = {
   onSubmit: (ticker: string) => void;
 };
-
-// const TickerInput2: React.FC<TickerInputProps> = ({ onSubmit }) => {
-//   const onKeyUp = (keyboardEvent: React.KeyboardEvent<HTMLInputElement>) => {
-//     if (keyboardEvent.code === 'Enter') {
-//       onSubmit(keyboardEvent.currentTarget.value);
-//     }
-//   };
-//   return (
-//     <label>
-//       Ticker:
-//       <input type="text" onKeyUp={onKeyUp} />
-//     </label>
-//   );
-// };
-
 class TickerInput extends React.Component<TickerInputProps> {
   constructor(props: TickerInputProps) {
     super(props);
